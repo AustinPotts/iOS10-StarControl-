989cd118de9c98d5d7e655e785f8f462eb9c7b6b
AustinPotts <pottsevolvements@gmail.com>
2019-10-10 15:18:10 -0400

    Configured Custom Control Class

---
+++ iOS10-Star/CustomControl.swift
@@ -0,0 +1,17 @@
+//
+//  CustomControl.swift
+//  iOS10-Star
+//
+//  Created by Austin Potts on 10/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value = 1
+    
+    
+}
